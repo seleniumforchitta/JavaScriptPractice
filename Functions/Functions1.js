4813a690e256c1d88f4f1b781bc51d6c60cc2c30
seleniumforchitta <seleniumforchitta@gmail.com>
2023-06-13 15:24:30 +0530

    chaning Function code

--- Functions/Functions1.js
+++ Functions/Functions1.js
@@ -13,5 +13,16 @@ greet(name)
 
 function add(a,b){
     console.log(a+b)
+    return a+b
 }
 add(4,5), add(4,55), add(4,555)
+let x = add(3,43)
+console.log(x)
+
+
+//Function as expression
+let y = function (num) { return num*num}
+console.log(y(5))
+
+let z = y(3)
+console.log(z)
