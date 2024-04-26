1eb8a956a627b8f1121f1b0a4295fa5ebb6d23a1
seleniumforchitta <seleniumforchitta@gmail.com>
2024-04-26 19:20:11 +0530

    Adding basic JS programs

--- Intro_And_ControlFlow/basics1_Variables.js
+++ Intro_And_ControlFlow/basics1_Variables.js
@@ -1,19 +1,34 @@
 console.log("Hello World");
 
+//Javascript Variables
 let a = 4
-const b = 5.67
-var c = "Ambuja"
+var b = 5.67
+// var is used in the older versions of JavaScript, let is the new way of declaring variables starting ES6 (ES2015).
 let d = true;
-const e = null;
-let f;
+let f; // If you use a variable without initializing it, it will have an undefined value.
 console.log(typeof(a));
 console.log(typeof(b));
-console.log(typeof(c));
 console.log(typeof(d));
-console.log(typeof(e));
 console.log(typeof(f));
-
+console.log(f); //undefined
 console.log(!d);
+
+let x=5, y=7, z=9;
+console.log(x);
+x = 3;
+console.log(x);
+
+//Javascript Constants 
+const c = "Ambuja"
+const e = null;
+console.log(typeof(c));
+console.log(typeof(e));
+// c = 10;  // Error! constant cannot be changed.
+console.log(c)
+
+//const p;  // Error! Missing initializer in const declaration.
+p = 5;
+console.log(p) 
 
 const t1 = Symbol(7878)
 console.log(t1)
@@ -35,10 +50,8 @@ console.log(typeof(student.fname))
 console.log(++student.age)
 
 
-console.log(2=='2')
-
-console.log(2==='2')
-
+console.log(2 =='2')
+console.log(2 ==='2')
 console.log(student.fname=='Ranjan')
 
 delete student.fname
---
+++ JavaScript_Programiz_Complete_Course/GettingStarted/jsVariables.js
@@ -0,0 +1,6 @@
+console.log(`I love javascript`);
+console.log("Javascript is fun");
+
+// Numeric Datatype
+console.log(8)
+console.log(80.5)
